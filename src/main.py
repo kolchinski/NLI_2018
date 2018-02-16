49cfdd54b44c954c329c8c23e6819b3601240571
Alex Kolchinski <y.a.kolchinski@gmail.com>
2018-02-16 17:03:51 -0500

    Updated data handling to load train, dev and test

--- src/main.py
+++ src/main.py
@@ -3,9 +3,9 @@ import dataman.wrangle as wrangle
 
 
 print("Hello toxic comments world!")
-dm = wrangle.DataManager(wrangle.load_train_data())
+dm = wrangle.DataManager(*wrangle.load_data())
 
 print('\n\n')
-print(dm.x1s[0])
-print(dm.x2s[0])
-print(dm.ys[0])
+print(dm.train_x1s[0])
+print(dm.train_x2s[0])
+print(dm.train_ys[0])

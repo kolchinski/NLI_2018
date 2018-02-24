1f7b7f0ad86835e89c5fa924d52010aa958e61a5
yaoliucs <yao.liu.chn@gmail.com>
2018-02-24 03:45:34 -0800

    add pretrained word embeddings

---
+++ src/models/load_embeddings.py
@@ -0,0 +1,79 @@
+import torch
+import torch.nn as nn
+
+from torch.autograd import Variable
+
+use_cuda = torch.cuda.is_available()
+
+import io
+import os
+import logging
+import array
+from tqdm import tqdm
+logger = logging.getLogger(__name__)
+
+PAD_token = 0
+EOS_token = 1
+UNK_token = 2
+
+#import models.load_embeddings as load_embeddings
+#import constants
+#model.net.encoder.embedding.weight.data = load_embeddings.load_embeddings(dm.vocab, constants.EMBED_DATA_PATH,
+#                                                                              args.embedding_size)
+
+def load_embeddings(vocab, path, d_embed, unk_init=torch.Tensor.zero_):
+    name = 'glove.6B.' + str(d_embed) + 'd.txt'
+    name_pt = name + '.pt'
+    path_pt = os.path.join(path,name_pt)
+
+    # Build pretrained embedding vectors
+    if os.path.isfile(path_pt):  # Load .pt file if there is any cached
+        logger.info('Loading vectors from {}'.format(path_pt))
+        itos, stoi, vectors, dim = torch.load(path_pt)
+    else:  # Read from Glove .txt file
+        path = os.path.join(path,name)
+        if not os.path.isfile(path):
+            raise RuntimeError('No files found at {}'.format(path))
+        try:
+            with io.open(path, encoding="utf8") as f:
+                lines = [line for line in f]
+        except:
+            raise RuntimeError('Could not read {} as format UTF8'.format(path))
+        logger.info("Loading vectors from {}".format(path))
+
+        itos, vectors, dim = [], array.array(str('d')), None
+
+        for line in tqdm(lines, total=len(lines)):
+            entries = line.rstrip().split(" ")
+            word, entries = entries[0], entries[1:]
+            if dim is None and len(entries) > 1:
+                dim = len(entries)
+            elif len(entries) == 1:
+                logger.warning("Skipping token {} with 1-dimensional "
+                               "vector {}; likely a header".format(word, entries))
+                continue
+            elif dim != len(entries):
+                raise RuntimeError(
+                    "Vector for token {} has {} dimensions, but previously "
+                    "read vectors have {} dimensions. All vectors must have "
+                    "the same number of dimensions.".format(word, len(entries), dim))
+            vectors.extend(float(x) for x in entries)
+            itos.append(word)
+        stoi = {word: i for i, word in enumerate(itos)}
+        vectors = torch.Tensor(vectors).view(-1, dim)
+        print(vectors.shape())
+        logger.info('Saving vectors to {}'.format(path_pt))
+        torch.save((itos, stoi, vectors, dim), path_pt)
+    # Look up vectors for words in vocabularin the pretrained vectors
+    vocab_vectors = torch.Tensor(vocab.n_words, dim).zero_()
+    #print(vocab.index2word)
+    for i, (_, token) in enumerate(vocab.index2word.items()):
+        #print(i,token,type(token),vocab.index2word[i],type(vocab.index2word[i]))
+        if i < 2:  # Skip the first 3 words PAD EOS UNK
+            continue
+        token = token.strip(',.')
+        if token in stoi:
+            vocab_vectors[i][:] = vectors[stoi[token]]
+        else:
+            vocab_vectors[i][:] = unk_init(torch.Tensor(1, dim))
+    return vocab_vectors

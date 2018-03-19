e783002ce848b5279aee9071118b3ad3eda0b1f7
Katherin Yu <yukatherin@Katherins-Mac-Pro.local>
2018-03-18 23:02:13 -0700

    paragraph level sampling

--- squad_preprocess/run_preprocess.py
+++ squad_preprocess/run_preprocess.py
@@ -1,17 +1,19 @@
 # python 3
 import json
-from run_preprocess import sample_squad_classif_dataset
+from sample_dataset import sample_squad_classif_dataset
 
 # load GloVe for vocab
+print('reading glove vectors')
 gl_vocab = set()
 with open('/Users/yukatherin/Downloads/glove.840B.300d.txt') as f:
     for i, line in enumerate(f.readlines()):
-        if i % 10000 == 0:
+        if i % 100000 == 0:
             print(i)
         gl_vocab.add(line.split()[0])
 assert 'happy' in gl_vocab
 
 # sample examples - train
+print('sampling train')
 path = '/Users/yukatherin/Downloads/train-v1.1.json'
 with open(path) as f:
     source_data = json.load(f)
@@ -19,6 +21,7 @@ _, _ = sample_squad_classif_dataset(gl_vocab=gl_vocab, source_data=source_data,
     output_path='/Users/yukatherin/Downloads/train_squad_classif.txt')
 
 # sample examples - dev
+print('sampling dev')
 path = '/Users/yukatherin/Downloads/dev-v1.1.json'
 with open(path) as f:
     source_data = json.load(f)
--- squad_preprocess/sample_dataset.py
+++ squad_preprocess/sample_dataset.py
@@ -18,8 +18,8 @@ def sample_squad_classif_dataset(gl_vocab, source_data, output_path, start_ratio
     stop_ai = int(round(len(source_data['data']) * stop_ratio))
     f = open(output_path, 'w')
     for ai, article in enumerate(tqdm(source_data['data'][start_ai:stop_ai])):
-        article_questions, article_answers = [], []
         for pi, para in enumerate(article['paragraphs']):
+            par_questions, par_answers = [], []
             for qa in para['qas']:
                 qa_answers = []
                 qi_tokenized = word_tokenize(qa['question'].lower())
@@ -44,23 +44,23 @@ def sample_squad_classif_dataset(gl_vocab, source_data, output_path, start_ratio
                     continue
                 longest_answer = max(qa_answers, key=len)
                 if len(longest_answer.split()) >= MinAnswerLen \
-                    and qi not in article_questions and longest_answer not in article_answers:  # dedupe
-                    article_questions.append(qi)
-                    article_answers.append(longest_answer)
+                    and qi not in par_questions and longest_answer not in par_answers:  # dedupe
+                    par_questions.append(qi)
+                    par_answers.append(longest_answer)
 
-        for i, a1 in enumerate(article_answers):
-            for a2 in article_answers:
-                if a1 not in a2 and a2 not in a1:  # not equal and not subset
-                    if np.random.rand() < 0.5:
-                        x1, x2 = a2, a1
-                        yi = 1
-                    else:
-                        x1, x2 = a1, a2
-                        yi = 0
-                    qi = article_questions[i]
-                    f.write('{}\t{}\t{}\t{}\n'.format(yi, x1, x2, qi))
-                    q.append(qi)
-                    a.append((a1, a2))
+            for i, a1 in enumerate(par_answers):
+                for a2 in par_answers:
+                    if a1 not in a2 and a2 not in a1:  # not equal and not subset
+                        if np.random.rand() < 0.5:
+                            x1, x2 = a2, a1
+                            yi = 1
+                        else:
+                            x1, x2 = a1, a2
+                            yi = 0
+                        qi = par_questions[i]
+                        f.write('{}\t{}\t{}\t{}\n'.format(yi, x1, x2, qi))
+                        q.append(qi)
+                        a.append((a1, a2))
     print('sampled {} examples'.format(len(q)))
     print('sampled unique questions {}'.format(len(set(q))))
     f.close()

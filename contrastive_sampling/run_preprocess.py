28a40c383c80f14ce0ee186fe99cbbbf8875fb46
yukatherin <yu.katherin@gmail.com>
2018-05-14 13:35:28 -0700

    nli contrastive sampling

---
+++ contrastive_sampling/run_preprocess.py
@@ -0,0 +1,50 @@
+# python 3
+import json
+import pandas as pd
+from sample_squad_contrastive import sample_squad_classif_dataset
+from sample_nli_contrastive import sample_nli_classif_dataset
+
+print('sampling train allnli')
+path = '/Users/yukatherin/Downloads/train_yoda'
+with open(path) as f:
+    source_data = pd.read_csv(f, sep='\t', header=None, quotechar='"')
+sample_nli_classif_dataset(source_data=source_data,
+    output_path='/Users/yukatherin/Downloads/train_allnli_classif.txt')
+
+
+print('sampling train allnli')
+path = '/Users/yukatherin/Downloads/tune_yoda'
+with open(path) as f:
+    source_data = pd.read_csv(f, sep='\t', header=None, quotechar='"')
+sample_nli_classif_dataset(source_data=source_data,
+    output_path='/Users/yukatherin/Downloads/tune_allnli_classif.txt')
+
+
+import sys; sys.exit(0)
+
+# load GloVe for vocab
+print('reading glove vectors')
+gl_vocab = set()
+with open('/Users/yukatherin/Downloads/glove.840B.300d.txt') as f:
+    for i, line in enumerate(f.readlines()):
+        if i % 100000 == 0:
+            print(i)
+        gl_vocab.add(line.split()[0])
+assert 'happy' in gl_vocab
+
+
+# sample examples - train
+print('sampling train squad')
+path = '/Users/yukatherin/Downloads/train-v1.1.json'
+with open(path) as f:
+    source_data = json.load(f)
+_, _ = sample_squad_classif_dataset(gl_vocab=gl_vocab, source_data=source_data,
+    output_path='/Users/yukatherin/Downloads/train_squad_classif.txt')
+
+# sample examples - dev
+print('sampling dev squad')
+path = '/Users/yukatherin/Downloads/dev-v1.1.json'
+with open(path) as f:
+    source_data = json.load(f)
+_, _ = sample_squad_classif_dataset(gl_vocab=gl_vocab, source_data=source_data,
+    output_path='/Users/yukatherin/Downloads/dev_squad_classif.txt')
---
+++ contrastive_sampling/sample_nli_contrastive.py
@@ -0,0 +1,54 @@
+import numpy as np
+from tqdm import tqdm
+import nltk
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
+
+
+def word_tokenize(tokens):
+    return [
+        token.replace("''", '"').replace("``", '"')
+        for token in nltk.word_tokenize(tokens)]
+
+
+# somewhat adapted from
+# https://github.com/allenai/bi-att-flow/blob/master/squad/prepro.py
+
+def sample_nli_classif_dataset(
+    source_data, output_path,
+    n_negative=3, sim_thres=0.4,
+    start_ratio=0.0, stop_ratio=1.0,
+):
+    """source_data: [label, premise, hypothesis] pd.DataFrame"""
+    contradiction_mask = np.where(source_data[0] == 'contradiction')[0]
+    print(len(contradiction_mask))
+
+    premise = source_data.iloc[contradiction_mask, 1].values
+    correct = source_data.iloc[contradiction_mask, 2].values
+    sampled_idx = np.random.permutation(n_negative * len(correct))
+
+    tfidf = TfidfVectorizer()
+    premise_tfidf = tfidf.fit_transform(premise)
+    correct_tfidf = tfidf.transform(correct)
+
+    with open(output_path, 'w') as f:
+        for i in range(len(premise)):
+            prem = premise[i]
+            true_hyp = correct[i]
+            sim_prem_hyp = linear_kernel(premise_tfidf[i], correct_tfidf[i])
+            if sim_prem_hyp > sim_thres:
+                continue
+            for j in range(n_negative):
+                neg_idx = sampled_idx[i * n_negative + j] % len(correct)
+                false_hyp = correct[neg_idx]
+                if np.random.rand() < 0.5:
+                    hyp1, hyp2 = true_hyp, false_hyp
+                    label = 0
+                else:
+                    hyp1, hyp2 = false_hyp, true_hyp
+                    label = 1
+                f.write('{}\t{}\t{}\t{}\n'.format(
+                    label, prem, hyp1, hyp2))
+
+    print('wrote sampled to {}'.format(output_path))
